12bfda4457dbb3002bbfc5353a42818cbe1ef842
AnkushPal0408 <90330974+AnkushPal0408@users.noreply.github.com>
2025-06-22 13:55:37 +0530

    Adding frontend to git

---
+++ src/components/Form.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios.post("http://localhost:3000/register", {
+      name,
+      email,
+      password,
+    });
+
+    // console.log("Form submitted:", { name, email, password });
+
+    // Show success message and hide form
+    setSubmitted(true);
+
+    // Clear form values
+    setName("");
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <div>
+      <h2>Customer Registration Form</h2>
+
+      {submitted ? (
+        <p>Form submitted successfully!</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Enter Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+          <br />
+          <input
+            type="email"
+            placeholder="Enter Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <br />
+          <input
+            type="password"
+            placeholder="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SuccessMsg.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SuccessMsg = () => {
+  return <div>SuccessMsg</div>;
+};
+
+export default SuccessMsg;
